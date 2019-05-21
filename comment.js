750183fa7361ce2199f175c1024ead28ebac1d82
mrbot <mrbot28652@gmail.com>
2019-05-21 12:17:27 +0530

    initial test

---
+++ comment.js
@@ -0,0 +1,8 @@
+//single line comments are like this and cannot have line braker 
+console.log("I just made a single line comment");
+
+/* this is a multi line commment 
+and it can have multi lines which means that 
+we can add line brakers in between this */
+
+console.log("multi line commends should have starting and ending /* and */");
---
+++ variables.js
@@ -0,0 +1,23 @@
+// defining a variable 
+
+var a;
+var b;
+
+// asiging values to a variable 
+
+a = 5;
+b = "admin account";
+
+//var is for the entire programme
+
+var xyz=134;
+
+// just for the context 
+
+let d = 67;
+
+
+//can never be changed
+
+cont pi=3.14;
+
